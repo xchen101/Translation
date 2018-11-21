8ebe994a14d609ecb4e2bcb80c71d88b6501294d
Xiaoli Chen <3393596+xchen101@users.noreply.github.com>
2018-11-21 17:03:48 +0100

    add SplitFile.py
    add SplitFile.py

---
+++ SplitFiles.py
@@ -0,0 +1,28 @@
+import re
+import sys
+import glob
+import math
+import random
+from array import *
+
+def ReadLogFileEntries (logfilename,outName,badlines,StoppLine) :
+    f = open(outName,'w')
+    for filename in glob.iglob(logfilename):
+        linenumber = 0
+        for line in open(filename, 'r'):
+           linenumber += 1
+           isGoodLine = True
+           for i in badlines:
+              if i == linenumber:
+                 isGoodLine = False
+           if isGoodLine == True:
+              line=line.split("\n")[0]
+              print(line,file=f)
+           if linenumber >= StoppLine:
+              linenumber = 0 
+
+ReadLogFileEntries("《二指禅》翻译专用.srt","English.srt",[1,2],4)
+#numbers in the square bracket is what's being taken out. 
+#ReadLogFileEntries("YGWS_Sub_full.txt","outputfileEnglish.txt",[3,5],6)
+#ReadLogFileEntries("YGWS_Sub_full.txt","outputfileChinese.txt",[4,5],6)
+
